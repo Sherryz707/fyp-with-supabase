feb0a92ed39a8708feb0f54b7496e3b3b7b3a88b
Sherryz707 <101007005+Sherryz707@users.noreply.github.com>
2025-05-30 11:19:39 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./app/app";
+import App from "./app/App";
 import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
